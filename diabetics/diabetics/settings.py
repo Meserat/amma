a994ab4b820a575525d623d098eadc7401e0e083
Meserat <hundekey3@gmail.com>
2021-10-20 09:47:06 +0300

    ch(deploy) set up deployment to heroku

--- diabetics/diabetics/settings.py
+++ diabetics/diabetics/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -163,3 +163,5 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ORIGIN_ALLOW_ALL = True
 ACCOUNT_LOGOUT_ON_GET = True
+
+django_heroku.settings(locals())
